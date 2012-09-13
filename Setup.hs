8d198eb8fbbad2ce0c4527c781659f35b8909c04
dennis <dennis@felsin9.de>
2012-09-13 10:53:23 +0000

    More helpful error message when trying to build using library-profiling

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,12 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.LocalBuildInfo
+import Distribution.PackageDescription (PackageDescription)
+
+main = defaultMainWithHooks $
+       simpleUserHooks { postConf = postConfHook (postConf simpleUserHooks) }
+
+postConfHook oldHook args flags descr buildInfo = case profFlag of
+  Flag True -> error "This library cannot be built using profiling. Try invoking cabal with the --disable-library-profiling flag."
+  _ -> oldHook args flags descr buildInfo
+  where profFlag = configProfLib $ configFlags buildInfo
