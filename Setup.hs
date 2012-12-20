812868a8a73495b41a28a1b0d57dcd48c4738d73
Joachim Breitner <mail@joachim-breitner.de>
2012-12-20 12:56:24 +0000

    Add ghci file providing :printHeap command

--- Setup.hs
+++ Setup.hs
@@ -3,8 +3,20 @@ import Distribution.Simple.Setup
 import Distribution.Simple.LocalBuildInfo
 import Distribution.PackageDescription (PackageDescription)
 
-main = defaultMainWithHooks $
-       simpleUserHooks { postConf = postConfHook (postConf simpleUserHooks) }
+import System.FilePath
+
+main = defaultMainWithHooks $ simpleUserHooks
+      { postInst = postInstHook (postInst simpleUserHooks)
+      , postConf = postConfHook (postConf simpleUserHooks)
+      }
+
+postInstHook oldHook args iflags pDesc lbi = do
+  let instDataDir = datadir $ absoluteInstallDirs pDesc lbi (fromFlag $ copyDest defaultCopyFlags)
+  putStrLn "To enable the GHCi integration, you have to load a ghci file in GHCi. To do this automatically when GHCi is started run:"
+  putStrLn $ "echo \":script " ++ (instDataDir </> "ghci") ++ "\" >> ~/.ghci"
+
+  oldHook args iflags pDesc lbi
+
 
 postConfHook oldHook args flags descr buildInfo = case profFlag of
   Flag True -> error "This library cannot be built using profiling. Try invoking cabal with the --disable-library-profiling flag."
