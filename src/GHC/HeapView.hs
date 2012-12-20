08f9f3bac1cef2c2ff6ff85b07e4dba8033b22e6
Joachim Breitner <mail@joachim-breitner.de>
2012-12-20 10:49:10 +0000

    Pretty-Print tuples

--- src/GHC/HeapView.hs
+++ src/GHC/HeapView.hs
@@ -604,6 +604,10 @@ isCons :: GenClosure b -> Maybe (b, b)
 isCons (ConsClosure { name = ":", dataArgs = [], ptrArgs = [h,t]}) = Just (h,t)
 isCons _ = Nothing
 
+isTup1 :: GenClosure b -> Maybe (b, b)
+isTup1 (ConsClosure { name = "(,)", dataArgs = [], ptrArgs = [h,t]}) = Just (h,t)
+isTup1 _ = Nothing
+
 isNil :: GenClosure b -> Bool
 isNil (ConsClosure { name = "[]", dataArgs = [], ptrArgs = []}) = True
 isNil _ = False
@@ -619,6 +623,8 @@ ppPrintClosure showBox prec c = case c of
         ["C#", show ch]
     _ | Just (h,t) <- isCons c -> addBraces (5 <= prec) $
         showBox 5 h ++ " : " ++ showBox 4 t
+    _ | Just (l,r) <- isTup1 c ->
+        "(" ++ showBox 0 l ++ "," ++ showBox 0 r ++ ")"
     ConsClosure {..} -> app $
         name : map (showBox 10) ptrArgs ++ map show dataArgs
     ThunkClosure {..} -> app $
